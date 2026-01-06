d3677cb623b1359f765ea3de21b3f7460f5a283a
visnukumar_vv <vishnu_kumar2@zohomail.in>
2026-01-06 08:19:33 +0000

    script menu running until Exit

---
+++ script4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true; do
+  echo "1.Disk 2.Memory 3.CPU 4.Exit"
+  read -p "Choice: " ch
+  case $ch in
+    1) df -h ;;
+    2) free -h ;;
+    3) uptime ;;
+    4) exit ;;
+    *) echo "Invalid choice" ;;
+  esac
+done
